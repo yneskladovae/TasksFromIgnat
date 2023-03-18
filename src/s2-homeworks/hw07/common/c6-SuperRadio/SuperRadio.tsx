dd2b57e52fed55eaa657570333a34fde9a3cf95f
yneskladovae <yneskladovae27@gmail.com>
2023-03-19 02:25:10 +0300

    fix SuperRadio

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -52,7 +52,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       className={finalRadioClassName}
                       type={'radio'}
                       // name, checked, value делают студенты
-                      name={o.name}
+                      name={o.value}
                       value={o.id}
                       checked={o.id == value}
 

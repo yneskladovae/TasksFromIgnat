c429a86bb91906dd4a51a8ebfaf3d2d2ac01e28c
yneskladovae <yneskladovae27@gmail.com>
2023-03-24 01:16:31 +0300

    add hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,14 +7,15 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            state = [...state].sort((a, b) => a.name > b.name ? 1 : -1)
-            return state // need to fix
+            return action.payload === 'up' ? [...state].sort((a, b) => a.name > b.name ? 1 : -1) :
+                [...state].sort((a, b) => a.name < b.name ? 1 : -1)
+            // state = [...state].sort((a, b) => a.name > b.name ? 1 : -1)
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age > 18)// need to fix
         }
         default:
-            return state.filter(el => el.)
+            return state
     }
 }
